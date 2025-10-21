fac3c9cb77816ea23f6523debe92320f8a6e3fa5
lawalemma24 <lawalemma24@gmail.com>
2025-10-21 16:41:49 +0100

    restaurant bank

--- src/Pages/Pickup.jsx
+++ src/Pages/Pickup.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react' 
-import axios from 'axios'
+// import { useState } from 'react' 
+// import axios from 'axios'
+
+// const Pickup = () => {
+//   const [caption,setCaption] = useState('initial data')
+//   const quote = ( ) => {
+//     axios.get('https://api.quotable.io/random').then( (res) => {console.log(res.data.content); setCaption((res.data.content))}).catch( (err) => {} );
+
+//   }
+//   return (
+//     <div>
+//         <button className='bg-red ml-[500px] mt-8 rounded-full text-2xl ' onClick={quote}>Generate Caption</button>
+//         {caption ? <h1>{caption}</h1> : null}
+
+
+//     </div>
+//   )
+// }
+
+// export default Pickup
+
+import React from 'react'
 
 const Pickup = () => {
-  const [caption,setCaption] = useState('initial data')
-  const quote = ( ) => {
-    axios.get('https://api.quotable.io/random').then( (res) => {console.log(res.data.content); setCaption((res.data.content))}).catch( (err) => {} );
-
-  }
   return (
-    <div>
-        <button className='bg-red ml-[500px] mt-8 rounded-full text-2xl ' onClick={quote}>Generate Caption</button>
-        {caption ? <h1>{caption}</h1> : null}
-
-
-    </div>
+    <div>Pickup</div>
   )
 }
 
